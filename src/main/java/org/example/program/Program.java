0dbd608b8befb5e22729981cb024a7626d223abe
summer-alice <stigma+github@disroot.org>
2023-08-26 18:11:59 +1000

    Initial commit

---
+++ src/main/java/org/example/program/Program.java
@@ -0,0 +1,12 @@
+package org.example.program;
+
+import java.util.Locale;
+
+public class Program
+{
+	public static void main(String[] args)
+	{
+		Locale defaultLocale = Locale.getDefault();
+		System.out.println(defaultLocale);
+	}
+}
